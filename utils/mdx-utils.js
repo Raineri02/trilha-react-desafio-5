cf3f8a2e470ee2ba7a4d8cfcab7b1e2011c2c84d
Raineri <asa.raineri20@gmail.com>
2024-11-30 21:34:32 -0300

    Concluido

--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -1,19 +1,18 @@
-import { api } from '../services/api'
-
-export const getPosts = async () => {
-    const {data} = await api.get('/posts'); 
-
-    if(data){
-        return data;
-    }
-
-    return []
-}
-
 export const getPostBySlug = async (id) => {
+    try {
+        // Fazendo a requisição para buscar o post pelo ID
+        const { data } = await api.get(`/posts/${id}`);
 
-    //TODO: BUSCAR UM POST EM ESPECIFICO.
-    //const {data} = await api.get(`/post?id=eq.${id}`)
+        // Retornando o post se encontrado
+        if (data) {
+            return data;
+        }
 
-    return {}
-}
+        // Retornando um objeto vazio caso nenhum post seja encontrado
+        return {};
+    } catch (error) {
+        console.error("Erro ao buscar o post:", error);
+        // Lidando com erros e retornando um objeto vazio
+        return {};
+    }
+};
